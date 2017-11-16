03728d52638c4fe8a78a8646472f39bea749a499
erikzaksf <erik.zak.sf@gmail.com>
2017-11-16 09:13:06 -0800

    add controller and routes for post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,10 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    render :index
+  end
+  def show
+    @post = Post.find(params[:id])
+    render :show
+  end
+end
